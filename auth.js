7f30732e854bf75ff1666519da6fef55317776e7
Focan Stefan <focanstefan2712@gmail.com>
2022-08-26 05:50:34 -0700

    Added google id and secret in .env

--- auth.js
+++ auth.js
@@ -2,17 +2,23 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 
-const GOOGLE_CLIENT_ID='711866024631-mtpobjso034luc5kfq485dmnuj1mv90v.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET='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';
+
+
+
+
+
 
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
 
+
+
+
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback: true,
   },
